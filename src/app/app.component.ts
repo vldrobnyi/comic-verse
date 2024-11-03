0e38b2311096c798086d6e5f27e5828bb57d5542
vldrobnyi <drobnyivladyslav@gmail.com>
2024-11-03 11:22:49 +0200

    feat(Header): add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExamplePdfViewerComponent } from './example-pdf-viewer/example-pdf-viewer.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ExamplePdfViewerComponent, NavMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
